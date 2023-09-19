d884ca4c314c3759eec5450a482b1174cbd43dd5
ulugbekpy <ulugbekpycpp@gmail.com>
2023-09-19 20:32:50 +0500

    a few line of codes

---
+++ loops/start.py
@@ -0,0 +1,54 @@
+# from turtle import Screen, Turtle
+
+# oyna = Screen()
+
+# oyna.title("Toshbaqa")
+
+# line = Turtle()
+# line.color("blue")
+# line.pensize(5)
+# line.hideturtle()
+
+# line.up()
+# line.goto(300, 300)
+# line.down()
+# line.goto(300, -300)
+# line.goto(-300, -300)
+# line.goto(-300, 300)
+# line.goto(300, 300)
+
+# koptok = Turtle(shape="circle")
+# koptok.color("red")
+
+# while True:
+#     koptok.goto(0, 0)
+
+# oyna.mainloop()
+
+
+ozgaruvchi = -2.3
+
+# types of variables
+
+# int
+
+a = 3
+b = 4
+
+# str string satrlar
+
+word = "w"
+
+# float o'nli kasr 2.334 -4.5
+
+rating = 4.7
+
+# bool boolean True/False
+
+my_bool = True
+my_bool = 2
+
+print(my_bool)
+print("kod ishlayapti")
+
+print(2 / 0)
