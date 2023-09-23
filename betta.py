a5b191b7de6a01c65b4dd50da42e73fef89addf4
ulugbekpy <ulugbekpycpp@gmail.com>
2023-09-23 11:53:48 +0500

    lesson with Azizbek

--- betta.py
+++ betta.py
@@ -1,4 +1,22 @@
-number = 23.37483
-print("{:.3f}".format(number))
-print("%.2f" % number)
-print("%d" % -2.3)
+# number = 23.37483
+# print("{:.3f}".format(number))
+# print("%.2f" % number)
+# print("%d" % -2.3)
+# a = 3
+
+# shart1 = True
+# shart2 = True
+# shart3 = True
+# shart4 = True
+
+# if shart1:
+#     if shart2:
+#         print("shart1 va shart2 bajariladi")
+#     elif shart3:
+#         print("shart1 va shart3 bajariladi")
+#     else:
+#         print("shart1 bajariladi")
+# elif shart4:
+#     print("shart4 bajariladi")
+# else:
+#     print("Hech bir shart bajarilmaydi")
