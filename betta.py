bae3513b261f6c551337b8d705d01164f13eb960
ulugbekpy <ulugbekpycpp@gmail.com>
2023-09-26 21:41:26 +0500

    day 4

--- betta.py
+++ betta.py
@@ -1,123 +1,67 @@
-# number = 23.37483
-# print("{:.3f}".format(number))
-# print("%.2f" % number)
-# print("%d" % -2.3)
-# a = 3
+import os
+import shutil
 
-# shart1 = True
-# shart2 = True
-# shart3 = True
-# shart4 = True
+# try:
+#     a = 10
+#     b = 0
+#     # print(a / b)
+# except Exception as e:
+#     print("Xato ketdi: {}".format(e))
+# finally:
+#     print("doimmi?")
 
-# if shart1:
-#     if shart2:
-#         print("shart1 va shart2 bajariladi")
-#     elif shart3:
-#         print("shart1 va shart3 bajariladi")
-#     else:
-#         print("shart1 bajariladi")
-# elif shart4:
-#     print("shart4 bajariladi")
+
+# if os.path.exists(paths):
+#     print("exists")
 # else:
-#     print("Hech bir shart bajarilmaydi")
-# from math import e
+#     print("No bro")
 
+# print(os.path.isfile(paths))
 
-# a = e
-# print(f""" {e} """)
+# with open("just.txt") as file:
+#     print(file.read())
 
+# textim = "\nPython programming"
 
-# my_list = ["and", "for", "loop", "again", "break"]
-# print(my_list)
-# my_list.sort(reverse=True)
-# print(my_list)
+# with open("just.txt", "a") as faylim:
+#     faylim.write(textim)
 
-# a = {"key1", "key2", "key3", "key4"}
-# b = "value"
+# with open("just.txt") as file:
+#     print(file.read())
 
-# newone = {}.fromkeys(a, b)
-# print(newone)
+# users
+# posts(title,content,image,)
+# faculties(title)
+# info images(faculty/info pk,imagefield)
+# partners (fullname,image,about)
+# books(image,name,file)
+# questions(name,phone number,email,question)
+# infos (aloqa,kollej tarixi)
+# comments(otzivlar)
 
+# import shutil
 
-# class Programming:
-#     def __init__(self, name) -> None:
-#         self.name = name
+# shutil.copyfile(".gitignore", "gitignore.docs")
 
+# src = "just.docs"
+# moving_place = "/home/ulugbekpy/just.docs"
 
-# class Java(Programming):
-#     pass
-
-
-# c = Programming("Python")
-
-# print(issubclass(Java, Programming))
-# print(isinstance(c, Programming))
-
-# # issubclass(A,B)   checks if A is subclass of B
-# # it  means A is child class B is parent class
-
-
-my_func = lambda a: a**2
-
-
-# a, b, c = list(map(float, input().split()))
-# print(type(a))
-# print(type(b))
-# print(type(c))
-
-# a = [1, 2, 3, 4, 5]
-# b = ["Facebook", "google", "amazon"]
-# print(type(zip(a, b)))
-# print(dict(zip(a, b)))
-
-
-# def filtrlayman(a):
-#     letters = ["a", "b"]
-#     if a in letters:
-#         return True
+# try:
+#     if os.path.exists(moving_place):
+#         print("there is already a file there!")
 #     else:
-#         return False
+#         os.replace(src, moving_place)
+#         print(src + " has been moved")
+# except FileNotFoundError:
+#     print(src + " was not found")
 
+# fayl = "trash.py"
+# folder = "deletable"
 
-# p = "F a a c e b o o k".split(" ")
-# print(p)
-# filtered = filter(filtrlayman, p)
-# print(p)
-# print(list(filtered))
-
-# mapped = tuple(map(my_func, a))
-# print(mapped)
-
-# word = "hello"
-# sliced = slice(2)
-# print(word[sliced])
-
-
-def fun_generator():
-    yield "Hello world!!"
-    yield "Geeksforgeeks"
-
-
-obj = fun_generator()
-
-# print(type(obj))
-
-# print(next(obj))
-# print("end")
-# print(next(obj))
-
-# for i in obj:
-#     print(i, end=" ")
-
-
-# def inf_sequence():
-#     num = 0
-#     while True:
-#         yield num
-#         num += 1
-#         if num == 1000:
-#             break
-
-
-# for i in inf_sequence():
-#     print(i, end=" ")
+# try:
+#     # os.remove(fayl)
+#     # os.rmdir(folder)
+#     shutil.rmtree(folder)
+#     print(f"{folder} removed")
+# except Exception as e:
+#     print("Exception: {}".format(e))
