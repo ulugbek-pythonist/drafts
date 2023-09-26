2500591ff86dcc5c4f2f47ba1ffd999b3e88df8b
ulugbekpy <ulugbekpycpp@gmail.com>
2023-09-26 17:21:51 +0500

    day 4

--- solutions/locked.py
+++ solutions/locked.py
@@ -45,3 +45,97 @@
 #     print("Palindrom")
 # else:
 #     print("Palindrom emas")
+
+
+# for i in range(1, 21):
+#     if i % 5 == 0:
+#         continue
+#     print(i)
+
+# lists --> ro'yxat
+# collection/sequence types
+# constructor --> a = list() | ([])
+
+# print(type(users))
+# a = "sams", "hixwj"
+# print(type(a))
+# a = (1, 2, 3, True, "string")
+
+# for i in "Python":
+#     print(i, end=";")
+
+# for user in users:
+#     print(user)
+
+# print("Python"[::-1])
+
+# print(users[3])
+# print(users[-2])
+
+# print(users[:2])
+# print(users[::-1])
+
+# print(users[1:4:2])
+# print(type(users[0:1]))
+# print(type(users[0]))
+
+# print(users)
+# users.append("Malika")
+# print("After appending Malika --> ", users)
+# users.insert(1, "Abdulloh")
+# print("After inserting Abdulloh --> ", users)
+# users.remove("Malika")
+# print("After removing Malika --> ", users)
+# a = "word"
+# print(a.upper())
+# print(a)
+# users.pop(1)
+# print(users)
+# users.sort()
+# users.insert(0, "Bahromjon")
+# print(users)
+# print(sorted(users))
+# print(users)
+
+users = ["Samandar", "Oyatulloh", "Ulug'bek", "Abdulaziz", "Xurshidbek"]
+# users.sort(reverse=True)
+# print(users)
+# users.sort()
+# print(users)
+# print(dir(list))
+
+# print("aaabbbcdef".count("c"))
+
+# print(users.count("samandar"))
+
+# print("hello " + "world")
+
+# people.clear()
+# del people
+# people = 1
+
+# people = users + accounts
+# print(people)
+# people.extend(["Asadbek", "Maliika", "Aziza"])
+# print(people)
+# print(people.index("Abdulloh", 0, 7))
+
+# tuple -->
+a = tuple()
+print(a)
+# print(dir(tuple))
+
+# print(sonlar.count(2))
+# print(sonlar.index(5, 3))
+# print(len(sonlar))
+
+# accounts = ["Abdulloh", "Bahromjon", "O'ktamjon"]
+
+# accounts[1] = "Samandar"
+
+# print(accounts)
+# sonlar = (2, 34, 5, 6, 7, 8, 5)
+# print(sonlar[3::-1])
+
+# a = ("a",)
+# print(type(a))
