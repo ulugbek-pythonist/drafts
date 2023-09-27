183a7f37f75cc1136acd05d7ef1baf8d7780c65d
ulugbekpy <ulugbekpycpp@gmail.com>
2023-09-27 22:39:47 +0500

    some codes from bro

---
+++ azizbek/amaliy/day5.py
@@ -0,0 +1,24 @@
+# d = {4, 1024, 5, 7, 1, 2}
+# print(d)
+# print(id(d))
+# c = {"c", "b", "a"}
+# print(id(c))
+# print(c)
+
+# # print(dir(set))
+# # d.update(c)
+# print(d)
+# da = isinstance(d, set)
+# print(da)
+
+# b = [1, 2, 3, 4, 5, 6, 7, 8]
+# a = slice(1, 7, 2)
+# print(b[a])
+
+# import os
+
+# try:
+#     os.remove("unititled.txt")
+#     print("File moved")
+# except Exception as e:
+#     print("Error: {}".format(e))
